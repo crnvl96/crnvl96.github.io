6b3d194f434e8bf3a3c50cab759dbf864978ccf7
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-12-14 22:38:43 -0300

    initial commit

---
+++ posts/2024-12-18-hello-world.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+author: Ádran Carnavale
+---
+
+![hello world](../assets/2024-12-19-00-21-16.jpeg)
+
+---
---
+++ site.hs
@@ -0,0 +1,106 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid (mappend)
+import Hakyll
+import Text.Pandoc.Highlighting
+import Text.Pandoc.Options (ReaderOptions (..), WriterOptions (..))
+
+--------------------------------------------------------------------------------
+config :: Configuration
+config =
+  defaultConfiguration
+    { destinationDirectory = "docs"
+    }
+
+main :: IO ()
+main = hakyllWith config $ do
+  match "templates/*" $ compile templateCompiler
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Home"
+              `mappend` defaultContext
+
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "CNAME" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "pages/*.html" $ do
+    route idRoute
+    compile $
+      getResourceBody
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
+
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
+
+  match "assets/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "files/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  create ["css/syntax.css"] $ do
+    route idRoute
+    compile $ do
+      makeItem $ styleToCss pandocCodeStyle
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archives"
+              `mappend` defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
+
+pandocCodeStyle :: Style
+pandocCodeStyle = breezeDark
+
+responsiveImage :: String -> String -> String
+responsiveImage alt src =
+  "<img src=\"" ++ src ++ "\" alt=\"" ++ alt ++ "\" class=\"img-fluid\">"
+
+pandocCompiler' :: Compiler (Item String)
+pandocCompiler' =
+  pandocCompilerWith
+    defaultHakyllReaderOptions
+    defaultHakyllWriterOptions
+      { writerHighlightStyle = Just pandocCodeStyle
+      }
